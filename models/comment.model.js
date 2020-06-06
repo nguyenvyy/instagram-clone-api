1c5d9ba0661cdb33e5d88694c58625aad3eb125b
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-07 00:02:55 +0700

    update comment model

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,12 +1,13 @@
 const { Schema, model } = require('mongoose')
+const { ObjectId, String } = Schema.Types
 const commentSchema = new Schema({
     byUser: {
-        type: Schema.Types.ObjectId,
+        type: ObjectId,
         ref: 'User',
         required: true
     },
     postId: {
-        type: Schema.Types.ObjectId,
+        type: ObjectId,
         required: true,
         index: true
     },
@@ -14,6 +15,25 @@ const commentSchema = new Schema({
         type: String,
         trim: true,
         required: true
+    },
+    replyToCommentId: {
+        type: ObjectId,
+        index: true
+    },
+    likeByUserId: {
+        type: [ObjectId],
     }
 }, {timestamps: true})
+
+commentSchema.methods.checkUserIsLiked = function(userId) {
+	return this.likeByIds ? this.likeByIds.includes(userId) : false;
+};
+
+commentSchema.method.getRelyComments = function(commentId, postId) {
+
+}
+commentSchema.method.getNumOfRelyComments = function(commentId, postId) {
+
+}
+
 module.exports = model('Comment', commentSchema)
