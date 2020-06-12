604cd6a00cf60150afe716c1485711d79208b36c
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-12 23:37:08 +0700

    update cors middleware

---
+++ api/middlewares/cors.js
@@ -0,0 +1,20 @@
+const cors = require("cors");
+const {env} = require('../../config/globals')
+let whitelist = [
+    'https://instagram-clone-d083b.web.app', 
+    'https://instagram-clone-d083b.firebaseapp.com']
+if(env.NODE_ENV === 'development') {
+    whitelist.push('http://localhost:3000')
+}
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log(origin)
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+const middleware = cors(corsOptions)
+module.exports = middleware
--- app.js
+++ app.js
@@ -4,30 +4,16 @@ const bodyParse = require("body-parser");
 const app = express();
 
 const {env} = require('./config/globals') 
-
-const initRestRoutes = require("./api/index");
 const { createConnection } = require("./db");
-const cors = require("cors");
+const initRestRoutes = require("./api/index");
+// cors middleware
+const corsMiddleware = require('./api/middlewares/cors')
 // error handler middleware
 const errorHandler = require("./api/middlewares/error-handler");
+
 // init connect  to mongodb atlas
 createConnection();
-// template engine
-const whitelist = {
-  origin: ['https://instagram-clone-d083b.web.app', 'https://instagram-clone-d083b.firebaseapp.com']
-};
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      console.log(origin)
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
-// app.use(cors());
+app.use(corsMiddleware);
 app.use(express.static("public"));
 app.use(bodyParse.urlencoded({ extended: false }));
 app.use(bodyParse.json());
